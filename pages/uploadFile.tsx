71e00f773afca5d254204759bcc46f02de02751e
Kanishk Rawat <rawat27kanishk@gmail.com>
2023-05-17 22:11:23 +0530

    Added Function for uploading files to firestore and storage

--- pages/uploadFile.tsx
+++ pages/uploadFile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import storage from "@/firebase/storage";
 import { useAuth } from "./contexts/auth";
@@ -6,21 +6,15 @@ import Upload from "./upload";
 import Login from "./Login";
 
 function UploadFile() {
-  // create function to upload file to firebase storage
-  const [file, setFile] = useState<File | null>(null);
   const { user } = useAuth();
-  const handleUpload = () => {
-    const storageRef = ref(storage, "images");
-    const fileRef = ref(storageRef, "image");
-    uploadBytesResumable(fileRef, file!).then((snapshot) => {
-      const url = getDownloadURL(snapshot.ref);
-      console.log(url);
-    });
-  };
+  useEffect(() => {
+    console.log("User", user);
+  }, [user]);
+
   if (!user) {
     return <Login />;
   }
-  return <Upload id="1" location="file" />;
+  return <Upload location="file" />;
 }
 
 export default UploadFile;
