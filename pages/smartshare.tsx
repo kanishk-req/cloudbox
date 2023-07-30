f49ce0c6b5e44fc4347a7fd4a5cfff02cd2ca6b1
Kanishk Rawat <rawat27kanishk@gmail.com>
2023-07-30 18:48:59 +0530

    Added SmartShare Link

--- pages/smartshare.tsx
+++ pages/smartshare.tsx
@@ -7,7 +7,7 @@ import storage from "@/firebase/storage";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { useAuth } from "./contexts/auth";
 import db from "@/firebase/firestore";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, setDoc, doc } from "firebase/firestore";
 
 export interface TempFilesData {
   file: File;
@@ -19,34 +19,51 @@ function Smartshare() {
   const [file, setFile] = useState<File | null>(null);
   const [urls, setUrl] = useState<TempFilesData[]>([]);
   const { user } = useAuth();
-  const uploadFileToFirestore = (downloadURL: string) => {
+  const randomId = Math.random().toString(36).substring(7);
+  const [smartId, setSmartId] = useState<string>("");
+  const [copyState, setCopyState] = React.useState(false);
+  const [ModalState, setModalState] = React.useState(false);
+  
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(smartId);
+    setCopyState(true);
+    setInterval(() => {
+      setCopyState(false);
+    }, 3000);
+  };
+  const uploadFileToFirestore = (downloadURL: string, SmartName: string) => {
     try {
-      addDoc(
-        collection(db, `User/${user?.uid}/ss-${new Date().toDateString().split(" ").join("-")}`),
-        {
-          name: file?.name,
-          size: file?.size,
-          type: file?.type,
-          time: dropDown.split(" ")[1] !== "week" ? parseInt(dropDown.split(" ")[0]) : parseInt(dropDown.split(" ")[0]) * 7,
-          url: downloadURL,
-          date: new Date().toDateString(),
-        }
-      );
+      const id = randomId;
+      setDoc(doc(db, `User/${user?.uid}/Smartshare/${id}`), {
+        name: SmartName,
+        time:
+          dropDown.split(" ")[1] !== "week"
+            ? parseInt(dropDown.split(" ")[0])
+            : parseInt(dropDown.split(" ")[0]) * 7,
+        date: new Date().toDateString(),
+      });
+      addDoc(collection(db, `User/${user?.uid}/Smartshare/${randomId}/files`), {
+        name: file?.name,
+        size: file?.size,
+        type: file?.type,
+        url: downloadURL,
+      });
+
       // setProgress(0);
       setFile(null);
-      setDropDown("off");
+      setSmartId(`${id}-${user?.uid}`);
     } catch (error: any) {
       // setError(error.message);
     }
   };
-  const uploadFileToStorage =  (
-    folderName: string,
-    name: string,
-    file: File
-  ) => {
+  const uploadFileToStorage = (SmartName: string, name: string, file: File) => {
     const storageRef = ref(
       storage,
-      `smartshare/${user?.uid}/${folderName}/${name}-${new Date().toDateString().split(" ").join("-")}}`
+      `smartshare/${user?.uid}/${SmartName}/${name}-${new Date()
+        .toDateString()
+        .split(" ")
+        .join("-")}}`
     );
     const uploadTask = uploadBytesResumable(storageRef, file);
     uploadTask.on(
@@ -62,7 +79,7 @@ function Smartshare() {
       async () => {
         try {
           const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-          uploadFileToFirestore(downloadURL);
+          uploadFileToFirestore(downloadURL, SmartName);
         } catch (error: any) {
           // setError(error.message);
         }
@@ -74,12 +91,13 @@ function Smartshare() {
     e.preventDefault();
     if (!file) return;
     if (!name.current?.value) return alert("Please enter a name");
-    if (dropDown === "off" || dropDown === "on") return alert("Please select a time");
+    if (dropDown === "off" || dropDown === "on")
+      return alert("Please select a time");
+    setModalState(true);
     urls.forEach((url) => {
       uploadFileToStorage(name.current!.value, url.file.name, url.file);
     });
     setUrl([]);
-    name.current.value = "";
   };
 
   useEffect(() => {
@@ -212,6 +230,55 @@ function Smartshare() {
           </div>
           <Preview urls={urls} />
         </div>
+        {ModalState && (
+          <div className="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] h-[50vh] w-[50vw] bg-white rounded-xl">
+            <div className="flex flex-col items-center justify-center h-full gap-4">
+              <h1 className="text-2xl">SmartLink</h1>
+              {smartId ? (
+                <p className="text-sm text-gray-500">
+                  Your SmartLink is ready to use
+                </p>
+              ) : (
+                <p className="text-sm text-gray-500">
+                  Your SmartLink is being generated
+                </p>
+              )}
+              {smartId ? (
+                <div className="flex flex-row items-center justify-center gap-4">
+                  <p>{smartId}</p>
+                  <button
+                    className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100  font-medium rounded-lg text-sm px-3 py-1.5 mr-2 mb-2 "
+                    onClick={() => {
+                      handleCopy();
+                    }}
+                  >
+                    {copyState ? "Copied" : "Copy"}
+                  </button>
+                </div>
+              ) : (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="w-8 h-8 mr-2 text-gray-200 animate-spin  fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
