69dbe1d0d8ec01ecbaa7a58b55c9c44e36630b49
Kanishk Rawat <rawat27kanishk@gmail.com>
2024-05-13 17:12:54 +0530

    Added Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,18 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-xxxxxxxxxx"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_measurement_Id}');
+        `}
+        </Script>
       </body>
     </Html>
   )
